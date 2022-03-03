a4ef6abe82fcc110ad57d6fe1f977d9d240cadc3
Slim <asa93@live.fr>
2022-03-03 18:27:22 +0100

    feat(landing): fix links footer

--- landing/components/Footer/Footer.tsx
+++ landing/components/Footer/Footer.tsx
@@ -15,9 +15,24 @@ const Footer = () => {
               lineHeight: "40px",
             }}
           >
-            <a href="">Terms</a>
-            <a href="">Whitepaper</a>
+            <a href="" target="_blank">
+              Terms
+            </a>
           </Col>
+          <Col>
+            <a
+              href="https://metapolis.gitbook.io/apinator-2042-v1/"
+              target="_blank"
+            >
+              Whitepaper
+            </a>
+          </Col>
+          <Col>
+            <a href="" target="_blank">
+              Twitter
+            </a>
+          </Col>
+          {/*
           <Col
             md={2}
             style={{
@@ -26,21 +41,14 @@ const Footer = () => {
               lineHeight: "40px",
             }}
           >
-            <a href="">Staking</a>
-            <a href="">Token</a>
+            <a href="" target="_blank">
+              Staking
+            </a>
+            <a href="" target="_blank">
+              Token
+            </a>
           </Col>
-        </Col>
-        <Col
-          md={2}
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            lineHeight: "40px",
-            paddingRight: "10%",
-          }}
-        >
-          <a href="">Discord</a>
-          <a href="">Twitter</a>
+            */}
         </Col>
       </Row>
       <Row className={styles.footerRow2}>
