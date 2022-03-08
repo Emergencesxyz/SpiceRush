00a956abf800e1d608cecb51a9e3c8520a62cb9e
Slim <asa93@live.fr>
2022-03-08 18:50:21 +0100

    fix(landing): title

--- landing/components/Presentation/Presentation.tsx
+++ landing/components/Presentation/Presentation.tsx
@@ -102,7 +102,6 @@ const Presentation: FunctionComponent<Props> = (props): JSX.Element => {
               <img
                 src="../pictures/Logo_Apinator_2042.png"
                 alt="logo-apinator"
-                style={{ width: "100%" }}
               />
             </div>
           </div>
