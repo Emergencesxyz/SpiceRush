f3edaa70cdd5d17e711ac3686c35678c8821b37b
yoansalvayre <yoan@eveasoft.com>
2022-04-14 17:16:24 +0200

    tag manager delete

--- game-interface/components/layouts/default.tsx
+++ game-interface/components/layouts/default.tsx
@@ -7,20 +7,6 @@ export default function Layout({ children }: React.PropsWithChildren<{}>) {
         <title>Spice Rush</title>
         <meta name="description" content="Spice Rush page" />
         <link rel="icon" href="/faviconSpiceRush.svg" />
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-40CDCTP9RQ"
-        ></script>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-ZH6N1L9LGN', { page_path: window.location.pathname });
-            `,
-          }}
-        />
       </Head>
 
       <main>{children}</main>
