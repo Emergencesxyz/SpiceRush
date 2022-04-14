d78bc30a1d1a5f0f960efbaecf84191d22fbee5c
yoansalvayre <yoan@eveasoft.com>
2022-04-14 17:15:42 +0200

    Layout: Metadata set up

---
+++ game-interface/components/layouts/default.tsx
@@ -0,0 +1,29 @@
+import Head from "next/head";
+
+export default function Layout({ children }: React.PropsWithChildren<{}>) {
+  return (
+    <>
+      <Head>
+        <title>Spice Rush</title>
+        <meta name="description" content="Spice Rush page" />
+        <link rel="icon" href="/faviconSpiceRush.svg" />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-40CDCTP9RQ"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-ZH6N1L9LGN', { page_path: window.location.pathname });
+            `,
+          }}
+        />
+      </Head>
+
+      <main>{children}</main>
+    </>
+  );
+}
--- game-interface/pages/_app.tsx
+++ game-interface/pages/_app.tsx
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Layout from "../components/layouts/default";
+import { Web3ReactProvider } from "@web3-react/core";
+import Web3 from "web3";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function getLibrary(provider: any): Web3 {
+  return new Web3(provider);
 }
 
-export default MyApp
+function Apinator({ Component, pageProps }: AppProps) {
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Web3ReactProvider>
+  );
+}
+
+export default Apinator;
