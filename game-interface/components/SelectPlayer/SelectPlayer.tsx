71206b159a06e51fc214cdd41e0987a8501a1287
yoansalvayre <yoan@eveasoft.com>
2022-05-03 14:34:57 +0200

    NFT cards init

--- game-interface/components/SelectPlayer/SelectPlayer.tsx
+++ game-interface/components/SelectPlayer/SelectPlayer.tsx
@@ -1,6 +1,14 @@
 import { useState, useEffect, useContext } from "react";
 import { useWeb3React } from "@web3-react/core";
-import { Col, Button, Dropdown, Spinner, Modal } from "react-bootstrap";
+import {
+  Col,
+  Button,
+  Dropdown,
+  Spinner,
+  Modal,
+  Card,
+  ListGroup,
+} from "react-bootstrap";
 import styles from "./SelectPlayer.module.scss";
 import { useMoralisWeb3Api } from "react-moralis";
 import Router from "next/router";
@@ -9,8 +17,6 @@ import BlockchainService from "../../services/BlockchainService";
 
 // for testing
 import { testTiles } from "../../borrar";
-
-
 
 const SelectPlayer = () => {
   const Web3Api = useMoralisWeb3Api();
@@ -27,7 +33,7 @@ const SelectPlayer = () => {
         const getUserNFTs = await Web3Api.account.getNFTsForContract({
           chain: "mumbai",
           address: account,
-          token_address: '0x680b20466bbc756E82Ce93d12E8179ecB688D9F5',
+          token_address: "0x680b20466bbc756E82Ce93d12E8179ecB688D9F5",
         });
 
         setUserNFTs(getUserNFTs?.result);
@@ -38,7 +44,7 @@ const SelectPlayer = () => {
   const selectNFT = async (id: string) => {
     const _character = await blockchainService.getCharacterInfo(+id);
 
-    console.log('info', _character)
+    console.log("info", _character);
     setCharacterInfo(_character);
 
     //load character info
@@ -46,8 +52,8 @@ const SelectPlayer = () => {
     //   setSpiceMined(await blockchainService.getSpiceMined(characterId));
     // }
 
-    Router.push("/game")
-  }
+    Router.push("/game");
+  };
 
   const ApeCards = () => {
     return userNFTs.map((e) => {
@@ -55,25 +61,44 @@ const SelectPlayer = () => {
         <div
           key={e.token_id}
           onClick={() => selectNFT(e.token_id)}
-          style={{
-            height: "300px",
-            width: "150px",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            border: "1px solid",
-            cursor: "pointer"
-          }}
+          className={styles.apecard}
         >
-          # {e.token_id}
+          <Card className={styles.content}>
+            <Card.Body style={{ width: "100%" }}>
+              <Card.Text>
+                <img src="/assets/nft.png" alt="nft ape" /># {e.token_id}
+              </Card.Text>
+              <Card.Link href="#">Life points: </Card.Link>
+              <Card.Text
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  width: "100%",
+                }}
+              >
+                <img src="/assets/ic_spice_ore.png" alt="spice ore" />
+                <div>
+                  <p>Spice Ore:</p>
+                  <h3>230039</h3>
+                </div>
+              </Card.Text>
+            </Card.Body>
+          </Card>
         </div>
-      )
-    })
-  }
+      );
+    });
+  };
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-      <h1>{account}</h1>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        padding: "50px",
+      }}
+    >
+      {/* <h1>{account}</h1> */}
       {ApeCards()}
     </div>
   );
