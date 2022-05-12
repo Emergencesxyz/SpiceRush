8637709014bc640b562701dddf8db9d11476d7d2
yoansalvayre <yoan@eveasoft.com>
2022-05-12 16:03:42 +0200

    change contract

--- landing/WalletHelpers/contractVariables.js
+++ landing/WalletHelpers/contractVariables.js
@@ -1,6 +1,6 @@
 // testnet
 export const contractAddressTestnet =
-  "0x3d241501C818B3E884361cE4f466B716e528d082";
+  "0x9bac8c583a86258C046FD4B14668F46CCd3d1e06";
 export const providerTestnet =
   "https://polygon-mumbai.infura.io/v3/03ce6aff8f9b46b8bb3ebf13b2900c71";
 
